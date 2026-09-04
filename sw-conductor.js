bebb0afc9a53f1816c09591aeada973482df1f05
IPE3 Engineering <ipe3ecuador@gmail.com>
2026-09-04 17:38:33 -0500

    Update sw-conductor.js

--- sw-conductor.js
+++ sw-conductor.js
@@ -1,25 +1,51 @@
-const CACHE_NAME = "mi-ruta-conductor-v8";
-const APP_SHELL = ["./conductor.html", "./manifest-conductor.webmanifest"];
+const CACHE_NAME = "mi-ruta-conductor-v9";
+const APP_SHELL = [
+  "./conductor.html",
+  "./manifest-conductor.webmanifest",
+  "./icon-conductor.svg"
+];
 
 self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(() => null));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .catch(() => null)
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   const req = event.request;
+
   if (req.mode === "navigate" || req.destination === "document") {
-    event.respondWith(fetch(req).then(res => {
-      const copy = res.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
-      return res;
-    }).catch(() => caches.match(req).then(cached => cached || caches.match("./conductor.html"))));
+    event.respondWith(
+      fetch(req)
+        .then(res => {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+          return res;
+        })
+        .catch(() =>
+          caches.match(req).then(cached => cached || caches.match("./conductor.html"))
+        )
+    );
     return;
   }
-  event.respondWith(caches.match(req).then(cached => cached || fetch(req)));
+
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req))
+  );
 });
