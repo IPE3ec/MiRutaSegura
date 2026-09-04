1dc6e1a80c984841516dca84e3099780f74abb42
IPE3 Engineering <ipe3ecuador@gmail.com>
2026-09-04 15:29:39 -0500

    Refactor service worker to use ASSETS array

--- sw-conductor.js
+++ sw-conductor.js
@@ -1,40 +1,26 @@
 const CACHE_NAME = "mi-ruta-segura-conductor-v1";
-const APP_SHELL = [
-  "./conductor.html",
-  "./manifest.webmanifest",
-  "./icon-conductor.svg",
+const ASSETS = [
+  "conductor.html",
+  "manifest-conductor.webmanifest",
+  "icon-conductor.svg",
   "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
   "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
 ];
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).catch(() => null)
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-  const req = event.request;
-  if (req.method !== "GET") return;
-
   event.respondWith(
-    caches.match(req).then(cached => {
-      if (cached) return cached;
-      return fetch(req).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => null);
-        return res;
-      });
-    }).catch(() => caches.match("./conductor.html"))
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
