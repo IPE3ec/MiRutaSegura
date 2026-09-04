932e4dc40833191d21f0aa92ebf762c5f41d57d3
IPE3 Engineering <ipe3ecuador@gmail.com>
2026-09-04 16:05:52 -0500

    Update sw-padre.js

--- sw-padre.js
+++ sw-padre.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "mi-ruta-segura-padre-v2";
-const ASSETS = ["padre.html", "manifest-padre.webmanifest", "icon-padre.svg", "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css", "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"];
+const CACHE_NAME = "mi-ruta-segura-padres-v3";
+const ASSETS = ["padre.html", "manifest-padres.webmanifest", "icon-padre.svg", "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css", "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"];
 self.addEventListener("install", event => { event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))); self.clients.claim(); });
 self.addEventListener("fetch", event => { event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
